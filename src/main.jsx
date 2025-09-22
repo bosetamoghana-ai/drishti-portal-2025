12fba8d58e61df721bcb05dde19e4f965fdabadf
bosetamoghana-ai <bose.tamoghana@gmail.com>
2025-09-22 20:58:58 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from '/App.jsx';
-import './index.css'
+// Using the '@' alias is a robust way to import
+import App from '@/App.jsx'
+import '@/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
